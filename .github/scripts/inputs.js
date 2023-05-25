d458ce24fc9701490ba2feca43596ed15694d682
AntonioGally <antonio.gally@gmail.com>
2023-05-25 16:50:39 -0300

    fix: :bug: Passing the input by env

--- .github/scripts/inputs.js
+++ .github/scripts/inputs.js
@@ -1,6 +1,5 @@
 const core = require('@actions/core');
 
-const myInput = core.getInput('inputName', { required: true });
-const myBooleanInput = core.getBooleanInput('booleanInputName', { required: true });
-core.info('look this log', { myInput, myBooleanInput });
-core.setOutput('outputKey', 'outputVal');
+const clickupStatus = process.env.CLICKUP_STATUS;
+
+core.info('look this log', { clickupStatus });
